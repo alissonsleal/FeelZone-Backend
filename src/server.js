ebfc20c329f165f54f9237058bd6ff5cfa8e2e0b
Alisson <alissonsleal@hotmail.com>
2020-07-27 01:13:52 -0300

    heroku should work now, i think

--- src/server.js
+++ src/server.js
@@ -3,15 +3,21 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    app.listen(3333);
+    console.log("listening on port 3333");
+  })
+  .catch((err) => console.log(err));
 
 //req.query = access query params(to filter)(POST)
 //req.params. = access route params(to edit/delete)(PUT)
@@ -20,4 +26,4 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+//app.listen(process.env.PORT || 3333);
