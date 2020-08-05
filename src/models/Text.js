ed8a7f4c9833ac323f728d914058d5a7c826ef99
Alisson <alissonsleal@hotmail.com>
2020-08-05 13:08:30 -0300

    ...

--- src/models/Text.js
+++ src/models/Text.js
@@ -4,10 +4,12 @@ const mongoosePaginate = require("mongoose-paginate-v2");
 const TextSchema = new mongoose.Schema(
   {
     /*
+
   user: {
     type: mongoose.Types.ObjectId,
     ref: "User",
   },
+  
   */
     title: { type: String, required: true },
     body: { type: String, required: true },
